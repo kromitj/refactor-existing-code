665afdf53867970a8d822b7a34b0f29cb9db52db
Mitch Kroska <kromitj@gmail.com>
2019-01-28 16:14:58 -0700

    extraction of the charge && reward points totals

--- rental/customer.js
+++ rental/customer.js
@@ -6,19 +6,30 @@ class Customer {
 	addRental(rental) {
 		this.rentals.push(rental)
 	}
+	getTotalCharge() {
+		let result = 0
+		this.rentals.forEach((rental) => {
+			result += rental.charge
+		})
+		return result
+	}
+	getTotalFrequentRenterPoints() {
+		let result = 0
+		this.rentals.forEach((rental) => {
+			result += rental.getFrequentRenterPoints()
+		})
+		return result
+	}
 	statement() {
 		let frequentRenterPoints = 0
-		let result = (`Rental Record  for ${this.name} \n`)
-		var totalAmount = 0
+		let result = (`Rental Record  for ${this.name} \n`)		
 		this.rentals.forEach((rental) => {
-			totalAmount += rental.charge
-			frequentRenterPoints += rental.getFrequentRenterPoints()
 			result += `\t ${rental.movie.title} \t ${rental.charge} \n`
-
 		})
-		result += `Amount Owed is ${totalAmount} \n`
-			result += `You Earned: ${frequentRenterPoints} frequent rental points \n`
+		result += `Amount Owed is ${this.getTotalCharge()} \n`
+		result += `You Earned: ${this.getTotalFrequentRenterPoints()} frequent rental points \n`
 		console.log(result)
+		return result
 	}	
 	
 }
