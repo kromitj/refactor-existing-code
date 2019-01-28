2fff24ff8aa83b425865b4fc008a926193d4bd7e
Mitch Kroska <kromitj@gmail.com>
2019-01-28 15:35:06 -0700

    Move Customer::amountFor() to Rental::getCharge()

--- rental/customer.js
+++ rental/customer.js
@@ -11,7 +11,7 @@ class Customer {
 		let result = (`Rental Record  for ${this.name} \n`)
 		var thisAmount = 0
 		this.rentals.forEach((rental) => {
-			let rentalAmount = this.amountFor(rental)
+			let rentalAmount = rental.getCharge()
 			thisAmount += rentalAmount 
 			result += `\t ${rental.movie.title} \t ${rentalAmount} \n`
 			frequentRenterPoints++
@@ -23,25 +23,8 @@ class Customer {
 		result += `Amount Owed is ${thisAmount} \n`
 			result += `You Earned: ${frequentRenterPoints} frequent rental points \n`
 		console.log(result)
-	}
-	amountFor(aRental) {
-		let result = 0
-		if (aRental.movie.priceCode === 0) {
-			result += 2
-			if (aRental.daysRented > 2) 
-			{
-				result += (aRental.daysRented - 2) * 1.5
-			}
-		} else if (aRental.movie.priceCode === 1) {
-			result += aRental.daysRented*3
-		} else if (aRental.movie.priceCode ===2) {
-			result += 1.5
-			if (aRental.daysRented > 3) { 
-				result += (aRental.daysRented - 3) * 1.5
-			}
-		}
-		return result
-	}
+	}	
+	
 }
 
 module.exports = Customer
--- rental/rental.js
+++ rental/rental.js
@@ -3,6 +3,25 @@ class Rental {
 		this.movie = movie 
 		this.daysRented = daysRented
 	}
+
+	getCharge() {
+		let result = 0
+		if (this.movie.priceCode === 0) {
+			result += 2
+			if (this.daysRented > 2) 
+			{
+				result += (this.daysRented - 2) * 1.5
+			}
+		} else if (this.movie.priceCode === 1) {
+			result += this.daysRented*3
+		} else if (this.movie.priceCode ===2) {
+			result += 1.5
+			if (this.daysRented > 3) { 
+				result += (this.daysRented - 3) * 1.5
+			}
+		}
+		return result
+	}
 }
 
 module.exports = Rental
